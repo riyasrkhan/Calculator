cf9d5f2cb7e7f6bc4c6015d8092eabe0e8cf9a3f
riyasrkhan <62215271+riyasrkhan@users.noreply.github.com>
2021-04-28 09:52:09 +0530

    Created
    Created

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-var firstvalue = document.getElementById("first");
-var secondvalue = document.getElementById("second");
-var finalvalue = document.getElementById("displayvalue");
-function addition() {
-    var first_value = firstvalue.value;
-    var second_value = secondvalue.value;
-    var calculateValue = parseInt(first_value) + parseInt(second_value);
-    finalvalue.innerHTML = calculateValue.toString();
-}
-function subtraction() {
-    var first_value = firstvalue.value;
-    var second_value = secondvalue.value;
-    var calculateValue = parseInt(first_value) - parseInt(second_value);
-    finalvalue.innerHTML = calculateValue.toString();
-}
-function multiplication() {
-    var first_value = firstvalue.value;
-    var second_value = secondvalue.value;
-    var calculateValue = parseInt(first_value) * parseInt(second_value);
-    finalvalue.innerHTML = calculateValue.toString();
-}
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+        var _this = this;
+        this.firstvalue = document.getElementById("first");
+        this.secondvalue = document.getElementById("second");
+        this.finalvalue = document.getElementById("displayvalue");
+        this.additionbtn = document.getElementById("addition");
+        this.additionbtn.addEventListener("click", function (e) { return _this.calculation('add'); });
+        this.subtractionbtn = document.getElementById("subtraction");
+        this.subtractionbtn.addEventListener("click", function (e) { return _this.calculation('sub'); });
+        this.multiplicationbtn = document.getElementById("multiplication");
+        this.multiplicationbtn.addEventListener("click", function (e) { return _this.calculation('multi'); });
+    }
+    Calculator.prototype.calculation = function (value) {
+        var first_value = this.firstvalue.value;
+        var second_value = this.secondvalue.value;
+        if (value == 'add') {
+            this.calculateValue = parseInt(first_value) + parseInt(second_value);
+        }
+        else if (value == 'sub') {
+            this.calculateValue = parseInt(first_value) - parseInt(second_value);
+        }
+        else if (value == 'multi') {
+            this.calculateValue = parseInt(first_value) * parseInt(second_value);
+        }
+        console.log(this.calculateValue);
+        this.finalvalue.innerHTML = this.calculateValue.toString();
+    };
+    return Calculator;
+}());
+// start the app
+new Calculator();
--- app.ts
+++ app.ts
@@ -1,25 +1,39 @@
+class Calculator {
+    firstvalue:any;
+    secondvalue:any;
+    finalvalue:any;
+    additionbtn:any;
+    subtractionbtn:any;
+    multiplicationbtn:any;
+    calculateValue:any;
+    constructor() {
+        this.firstvalue = document.getElementById("first") as HTMLInputElement;
+        this.secondvalue = document.getElementById("second") as HTMLInputElement;
+        this.finalvalue = document.getElementById("displayvalue") as HTMLSpanElement;
+        this.additionbtn = document.getElementById("addition");
+        this.additionbtn.addEventListener("click", (e:Event) => this.calculation('add'));
+        this.subtractionbtn = document.getElementById("subtraction");
+        this.subtractionbtn.addEventListener("click", (e:Event) => this.calculation('sub'));
+        this.multiplicationbtn = document.getElementById("multiplication");
+        this.multiplicationbtn.addEventListener("click", (e:Event) => this.calculation('multi'));
+      }
 
-var firstvalue = document.getElementById("first") as HTMLInputElement;
-var secondvalue = document.getElementById("second") as HTMLInputElement;
-
-
-var finalvalue = document.getElementById("displayvalue") as HTMLSpanElement;
-
-    function addition(){
-    let first_value : string = firstvalue.value;
-    let second_value : string = secondvalue.value;
-    let calculateValue = parseInt(first_value) + parseInt(second_value);
-    finalvalue.innerHTML = calculateValue.toString();
+    calculation(value:any){
+        let first_value : string = this.firstvalue.value;
+        let second_value : string = this.secondvalue.value;
+        if(value == 'add'){
+            this.calculateValue = parseInt(first_value) + parseInt(second_value);
+        }else if(value == 'sub'){
+            this.calculateValue = parseInt(first_value) - parseInt(second_value);
+        }else if(value == 'multi'){
+            this.calculateValue = parseInt(first_value) * parseInt(second_value);
+        }
+        
+        console.log(this.calculateValue)
+        this.finalvalue.innerHTML = this.calculateValue.toString();
     }
-    function subtraction(){
-    let first_value : string = firstvalue.value;
-    let second_value : string = secondvalue.value;
-    let calculateValue = parseInt(first_value) - parseInt(second_value);
-    finalvalue.innerHTML = calculateValue.toString();
-    }
-    function multiplication(){
-    let first_value : string = firstvalue.value;
-    let second_value : string = secondvalue.value;
-    let calculateValue = parseInt(first_value) * parseInt(second_value);
-    finalvalue.innerHTML = calculateValue.toString();
-    }
+
+
+}
+// start the app
+new Calculator();    
